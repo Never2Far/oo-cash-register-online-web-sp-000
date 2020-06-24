8620ef4620fa033188672d76a60507d1c4d3e07a
Seth Near <seth.j.near@gmail.com>
2020-06-24 03:16:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,39 @@
 class CashRegister
 
+def initialize(emp_discount = 0)
+  @total = 0
+  @discount = emp_discount
+end
 
 
+def total
+  
+end
 
+
+def add_item(title, price, qty*)
+  total_backup = @total
+end
+
+
+def apply_discount
+end
+
+def 
+end
+
+def 
+end
+
+def 
+end
+
+def 
+end
+
+def 
+end
+
+end
 
 end
